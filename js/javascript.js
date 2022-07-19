b79db329cb17d4fbf2d1c008a195a4ea2ce71a4f
Germán Waisvol <german_waisvol@hotmail.com>
2022-07-19 00:03:42 -0300

    Primer desafío entregable

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,8 @@
-
-let Numero =parseInt(prompt("Ingrese el número de veces que desea repetir la palabra hola. El programa termina si ingresa un número mayor a 20"));
-
-if( isNaN(Numero) )
-{
-    prompt("Debe ingresar un número");
-}
-else
-{
-
-while ( Numero < 20 )
-{
-for (let i=0 ; i<Numero ; i ++){ 
-alert("Hola");
-}
-
-Numero = prompt("Ingrese el número de veces que desea repetir la palabra Hola");
-
-if( isNaN(Numero) )
-{
-    prompt("Debe ingresar un número");
-    break;
-}
-
-}
-
-}
+let valor = 0;
+do {
+    valor = prompt("Ingrese un numero. Se repetirá dicha cantidad de veces la palabra hola");
+    for (let i = 0; i < valor; i++) {
+        //console.log("Hola");
+        alert("Hola");
+    }
+} while (parseInt(valor));
